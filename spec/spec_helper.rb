27fe394a0dffa78f7b5bc6a93300e701fb67278d
danof-1105 <n-takeda@philureka.com>
2020-12-19 15:56:15 +0900

    first commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # テストを最後まで走らせる
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true unless meta.has_key?(:aggregate_failures)
+  end
+end
