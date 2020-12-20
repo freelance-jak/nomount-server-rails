a1af7a0e81b30a89252d4da0e8e4ca3eee318dd4
danof-1105 <n-takeda@philureka.com>
2020-12-20 13:59:24 +0900

    memberのroutes修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :events, except: :show
-    resources :members, except: :show
+    resources :members, only: :create
     mount_devise_token_auth_for "User", at: "auth", controllers: {
       registrations: "api/auth/registrations",
     }
