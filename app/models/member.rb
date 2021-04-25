c3c4e8d65135d7f8751a41cb5dc0279d9ab02996
danof-1105 <n-takeda@philureka.com>
2021-04-25 16:41:25 +0900

    memberモデルのリレーションを修正

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Member < ApplicationRecord
-  has_many :events, dependent: :destroy
+  has_many :event_members, dependent: :destroy
   has_many :events, through: :event_members, dependent: :nullify
 end
