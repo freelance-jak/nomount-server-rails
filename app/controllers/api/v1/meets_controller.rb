e1639604e58a9803f3562de628c3618e777ad202
danof-1105 <n-takeda@philureka.com>
2021-04-25 16:41:46 +0900

    特定のイベントのメンバーを返すAPIのモック作成

--- app/controllers/api/v1/meets_controller.rb
+++ app/controllers/api/v1/meets_controller.rb
@@ -1,4 +1,19 @@
 class Api::V1::MeetsController < ApplicationController
+
+  # NOTE: とりあえずのモック
   def show
+    event = Event.first
+    event_members = EventMember.where(event: event).includes(:event, :member)
+    join_members = event_members.map do |em|
+      {
+        member:
+        {
+          connpass_account: em.member.connpass_account,
+          name: em.member.name,
+        },
+        is_joining: em.status
+      }
+    end
+    render json: { meets: join_members }
   end
 end
